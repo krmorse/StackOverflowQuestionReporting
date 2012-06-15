fb1a6aad62acff10d70462496b32772bd0e9494d
Charles <Ferentchak@gmail.com>
2012-06-15 14:27:38 -0600

    bucketz

--- App.js
+++ App.js
@@ -7,23 +7,36 @@ Ext.define('CustomApp', {
     _bucketByDay:{
 
     },
+    _bucketByMonth:{
+
+    },
 
     _displayData:function() {
         console.log(this.results);
         Ext.each(this.results, function(question) {
             var date = Ext.Date.format((Ext.Date.parse(question.creation_date, "U")),"c").split('T')[0];
-            console.log(date);
             if (this._bucketByDay[date]) {
                 this._bucketByDay[date]++;
             }
             else {
                 this._bucketByDay[date] = 1;
             }
+
+            var monthString = date.substring(0,7);
+            console.log(monthString);
+            if (this._bucketByMonth[monthString]) {
+                this._bucketByMonth[monthString]++;
+            }
+            else {
+                this._bucketByMonth[monthString] = 1;
+            }
+
+
         }, this);
-        var sorted = Ext.Object.getKeys(this._bucketByDay).sort();
+        var sorted = Ext.Object.getKeys(this._bucketByMonth).sort();
         Ext.each(sorted,function(key){
             this.add({
-                html:key+" "+this._bucketByDay[key]
+                html:key+" "+this._bucketByMonth[key]
             });
         },this);
 
